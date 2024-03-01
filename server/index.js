84097e62686c76cffd930bf3dd96ce3a90ee0763
Titou-Dh <titoudh1@gmail.com>
2024-03-01 22:55:59 +0100

    updated the origin

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors(
     {
-        origin: 'http://localhost:3000',
+        origin: 'http://crud-app-mern-api-three.vercel.app',
         methods:["POST", "GET", "DELETE"],
         credentials: true
     }
